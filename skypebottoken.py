6a20be3202f33686115462c557c81238cb834fb4
Adrian Suciu <adrian.suciu1988@gmail.com>
2019-10-07 20:33:34 +0300

    added app_id loading logic
    Signed-off-by: Adrian Suciu <adrian.suciu1988@gmail.com>

--- skypebottoken.py
+++ skypebottoken.py
@@ -1,5 +1,22 @@
 import os
+from common import log
+
 port = os.environ.get('PORT', 8000)
 
-app_id = os.environ.get('app_id', "app_id")
-app_secret = os.environ.get('app_secret', "app_secret")
+app_id = ""
+app_secret = ""
+
+try:
+    with open("skypetoken", "r") as f:
+        content = f.read().splitlines()
+
+    try:
+        app_id = content[0]
+        app_secret = content[1]
+    except:
+        log("skypetoken Empty")
+except:
+    log("skypetoken not found getting tokens from envvar")
+
+app_id = os.environ.get('app_id', app_id)
+app_secret = os.environ.get('app_secret', app_secret)
